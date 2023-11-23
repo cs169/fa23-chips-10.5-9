ae5918ed9692ac4776dfd1dab3f3642848554eaa
Alejandro Dominguez <alejodomcar@berkeley.edu>
2023-11-23 02:36:22 +0000

    start writing cucumber tests, create basic fix

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -24,6 +24,7 @@ class MapController < ApplicationController
     handle_county_not_found && return if @state.nil?
 
     @county_details = @state.counties.index_by(&:std_fips_code)
+    redirect_to search_representatives_path
   end
 
   private
