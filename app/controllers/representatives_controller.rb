67581c84be9cf6c2b61510ecfa4005681fdb4c4e
Zhihan Cheng <czh@berkeley.edu>
2023-11-23 00:20:44 +0000

    save progress, 13 coverage

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -11,16 +11,6 @@ class RepresentativesController < ApplicationController
 
   def show
     @id = params[:id]
-    # @rep_name = "Rep Name"
-    # @rep_ocdid = "1111111"
-    # @rep_title = "Rep title"
-    # @rep_street = "Rep street"
-    # @rep_city = "rep city"
-    # @rep_state = "rep state"
-    # @rep_zip = "94704"
-    # @rep_party = "rep party"
-    # @rep_photo = "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/2800/Joey-Tribbiani.Friends.webp"
-
     @representative = Representative.find(@id)
 
     return unless @representative
