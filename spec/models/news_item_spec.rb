7f7b8175ea6ac2582e0b31e5419383164aa93e5f
Mike Martinez <mike.martinez@berkeley.edu>
2023-11-29 00:05:28 +0000

    Removed redundant validation code present in news_item.rb and news_item_spec.rb

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -4,17 +4,10 @@ require 'rails_helper'
 
 RSpec.describe NewsItem, type: :model do
   let(:representative) { create(:representative) }
-  let(:valid_issues) do
-    ['Free Speech', 'Immigration', 'Terrorism', 'Social Security and Medicare', 'Abortion', 'Student Loans',
-     'Gun Control', 'Unemployment', 'Climate Change', 'Homelessness', 'Racism', 'Tax Reform', 'Net Neutrality',
-     'Religious Freedom', 'Border Security', 'Minimum Wage', 'Equal Pay']
-  end
 
   it 'is valid with a valid issue' do
-    valid_issues.each do |issue|
-      news_item = described_class.new(issue: issue, representative: representative)
-      expect(news_item).to be_valid
-    end
+    news_item = described_class.new(issue: 'Free Speech', representative: representative)
+    expect(news_item).to be_valid
   end
 
   it 'is invalid with an invalid issue' do
