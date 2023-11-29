7adb8eb77d1f6dcfa9a525d165f89affd289c577
Mike Martinez <mike.martinez@berkeley.edu>
2023-11-29 00:13:23 +0000

    Refactored some code in news_item_spec.rb since I was getting errors that it was too similar

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -5,13 +5,16 @@ require 'rails_helper'
 RSpec.describe NewsItem, type: :model do
   let(:representative) { create(:representative) }
 
+  before do
+    @valid_news_item = described_class.new(issue: 'Free Speech', representative: representative)
+    @invalid_news_item = described_class.new(issue: 'Invalid Issue', representative: representative)
+  end
+
   it 'is valid with a valid issue' do
-    news_item = described_class.new(issue: 'Free Speech', representative: representative)
-    expect(news_item).to be_valid
+    expect(@valid_news_item).to be_valid
   end
 
   it 'is invalid with an invalid issue' do
-    news_item = described_class.new(issue: 'Invalid Issue', representative: representative)
-    expect(news_item).not_to be_valid
+    expect(@invalid_news_item).not_to be_valid
   end
 end
